f9ee985f615b744374fe88300d76c84d0aa584ae
umut karaytu <128383118+Karaytumut20@users.noreply.github.com>
2024-03-07 20:42:24 +0300

    dinamik verileri olusturuldu

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
-const fn = function () {
-    console.log('functıon workıng')
-};
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const ejs = require('ejs');
+const Photo = require('./models/Photo');
+
+const app = express();
+
+//connectDB
+mongoose.connect('mongodb://127.0.0.1/pcat-test-db');
+
+// VIEW ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARE
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTES
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/'); // Corrected typo
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server ${port} portunda dinleniyor`);
+});
